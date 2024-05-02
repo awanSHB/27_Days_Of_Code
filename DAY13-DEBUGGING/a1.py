21b55d21d51150f23fbb105fcf49b00c61c7e56b
awanSHB <139391360+awanSHB@users.noreply.github.com>
2024-05-02 15:22:58 +0500

    Add files via upload

---
+++ DAY13-DEBUGGING/a1.py
@@ -0,0 +1,16 @@
+# from random import randint
+# lost = ['0', '1', '2', '3']
+# ch = randint[1, 6]
+# print(ch)
+
+# i = input("Enter your age")
+# if i >10:
+#     print("You are teenager now")
+
+pages = 0
+word_per_page = 0
+words = int(input("Number of pages: "))
+word_per_page == input("Number of words :") # probsi is here
+total_words = word_per_page*words
+print(total_words)
+
---
+++ DAY13-DEBUGGING/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+from random import randint
+lost = ['0', '1', '2', '3']
+ch = randint(lost)
+print(ch)
